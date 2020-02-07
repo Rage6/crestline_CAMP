1498d87855526c550c833ec01cdfdbb047c9dcde
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2020-02-07 16:48:38 -0500

    set up html, css before php stuff

--- html/login.php
+++ html/login.php
@@ -3,31 +3,47 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Crestline C.A.M.P.</title>
+    <title>Login | Crestline C.A.M.P.</title>
     <!-- Width: 0px to 360px (Default CSS) -->
-    <link rel="stylesheet" type="text/css" href="css/home/360px_home.css" />
+    <link rel="stylesheet" type="text/css" href="css/login/360px_login.css" />
     <!-- Width: 361px to 375px -->
-    <link rel="stylesheet" type="text/css" media="screen and (min-width: 361px) and (max-width: 375px)" href="css/home/375px_home.css" />
+    <link rel="stylesheet" type="text/css" media="screen and (min-width: 361px) and (max-width: 375px)" href="css/login/375px_login.css" />
     <!-- Width: 376px to 414px -->
-    <link rel="stylesheet" type="text/css" media="screen and (min-width: 376px) and (max-width: 414px)" href="css/home/414px_home.css"/>
+    <link rel="stylesheet" type="text/css" media="screen and (min-width: 376px) and (max-width: 414px)" href="css/login/414px_login.css"/>
     <!-- Width: 415px to 768px -->
-    <link rel="stylesheet" type="text/css" media="screen and (min-width: 415px) and (max-width: 768px)" href="css/home/768px_home.css"/>
+    <link rel="stylesheet" type="text/css" media="screen and (min-width: 415px) and (max-width: 768px)" href="css/login/768px_login.css"/>
     <!-- Width: 769px to 1366px -->
-    <link rel="stylesheet" type="text/css" media="screen and (min-width: 769px) and (max-width: 1366px)" href="css/home/1366px_home.css"/>
+    <link rel="stylesheet" type="text/css" media="screen and (min-width: 769px) and (max-width: 1366px)" href="css/login/1366px_login.css"/>
     <!-- Width: 1367px to 1440px -->
-    <link rel="stylesheet" type="text/css" media="screen and (min-width: 1367px) and (max-width: 1440px)" href="css/home/1440px_home.css"/>
+    <link rel="stylesheet" type="text/css" media="screen and (min-width: 1367px) and (max-width: 1440px)" href="css/login/1440px_login.css"/>
     <!-- Width: 1441px and above -->
-    <link rel="stylesheet" type="text/css" media="screen and (min-width: 1441px)" href="css/home/1920px_home.css"/>
+    <link rel="stylesheet" type="text/css" media="screen and (min-width: 1441px)" href="css/login/1920px_login.css"/>
     <!-- All of the fonts -->
     <link href="https://fonts.googleapis.com/css?family=Acme|Belleza|Darker+Grotesque|Francois+One|Julius+Sans+One|Lexend+Exa|Pontano+Sans|Rubik&display=swap" rel="stylesheet">
     <!-- For JS and jQuery -->
     <?php echo($jquery); ?>
     <!-- The main, custom JS -->
-    <script src="js/home.js"></script>
-    <!-- The source of the Google Map -->
-    <!-- <script async defer src="https://maps.googleapis.com/maps/api/js?key= &callback=initMap"></script> -->
+    <script src="js/login.js"></script>
   </head>
   <body>
-    
+    <div class="title">
+      <div>C.A.M.P.</div>
+      <div>Crestline Assistance & Ministries Program</div>
+    </div>
+    <div class="wholeBody">
+      <div class="loginBox">
+        <form method="POST">
+          <div>
+            <input type="text" name="username" placeholder="Username" />
+          </div>
+          <div>
+            <input type="password" name="password" placeholder="Password" />
+          </div>
+          <div>
+            <input class="loginEnter" type="submit" name="login" value="ENTER" />
+          </div>
+        </form>
+      </div>
+    </div>
   </body>
 </html>
--- index.php
+++ index.php
@@ -14,6 +14,9 @@
     if (isset($_GET['admin'])) {
       require_once("php/admin.php");
       require_once("html/admin.php");
+    } elseif (isset($_GET['login'])) {
+      require_once("php/login.php");
+      require_once("html/login.php");
     } else {
       require_once("php/home.php");
       require_once("html/home.php");
