da76dadda03c128892e7c63a3a1aa57aeb5d960d
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2020-02-12 15:35:55 -0500

    lead_admin.php checks the tokens

--- admin/lead_admin.php
+++ admin/lead_admin.php
@@ -1,16 +1,25 @@
 <?php
 
+// The user id will be used to compare the dB's token to the user's token
 if (isset($_SESSION['userId'])) {
-  // if () {
-  //
-  // } else {
-  //
-  // };
+  $compareTokenStmt = $pdo->prepare("SELECT token FROM users WHERE user_id=:ui");
+  $compareTokenStmt->execute(array(
+    ':ui'=>htmlentities($_SESSION['userId'])
+  ));
+  $databaseToken = $compareTokenStmt->fetch(PDO::FETCH_ASSOC)['token'];
+  if ($databaseToken != $_SESSION['token']) {
+    session_destroy();
+    $_SESSION['message'] = "<div class='loginMssg' style='color: red'>Nice try.</br>Your tokens do not match</div>";
+    header('Location: ../login/login.php');
+    exit;
+    return false;
+  };
 } else {
   session_destroy();
+  $_SESSION['message'] = "<div class='loginMssg' style='color: red'>Nice try.</br>You must login before entering CAMP's Staff Center</div>";
   header('Location: ../login/login.php');
   exit;
-  return true;
+  return false;
 };
 
 if (isset($_POST['exitClick'])) {
